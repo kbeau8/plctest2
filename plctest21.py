281c37256af976ecc8e34855697353e4952262ad
kbeau8 <124460486+kbeau8@users.noreply.github.com>
2023-04-21 17:13:10 -0400

    Add files via upload

---
+++ plctest21.py
@@ -0,0 +1,15 @@
+DataType int, float;
+int main() {
+    int x = 5;
+    float y = 3.14;
+    while (x < 10 && y >= 2.0){
+    if (x% 2 == 0) {
+    x = x+1;}
+    else {
+    x = x*2;
+    }
+
+    
+    }
+    return x;
+}
